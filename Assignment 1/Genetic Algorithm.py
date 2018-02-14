d51de334d13a0e37e0ad8f58a713fdcbef1e99d8
Elisha Waugh <elishawaugh@outlook.com>
2018-02-13 19:15:11 -0900

    This needs to have the fitness function fixed.

--- Assignment 1/Genetic Algorithm.py
+++ Assignment 1/Genetic Algorithm.py
@@ -11,7 +11,7 @@ from random import *
 
 #   Global Variables
 population_size = 10
-k = 0
+k =  0 # size of smallest string
 smallest_string = ""
 largest_string = ""
 pop = [] 
@@ -46,18 +46,65 @@ def fitness(sample, i, j, fit_score):
     else:
         return fitness(sample, i, j + 1, fit_score)
 
-def mating_pool():
-    total_fitness = 0
+def mating_pool(new_pop_size):
+    total_fitness = []
     roulette_wheel = []
+    result = []
     for i in range(0, population_size):
-        total_fitness =+ pop[i][k+1]
-    
-    return x
+        total_fitness.append(pop[i][k+1])
+    for i in range(0, population_size):
+        for j in range(0, total_fitness[i]):
+            roulette_wheel.append(pop[i])
+    for i in range(0, new_pop_size):
+        result.append(roulette_wheel[randint(0, len(roulette_wheel)-1)])
+    return result
+
+def breed(mating_pool):
+    mate_match = []
+    result = []
+    for i in range(0, len(mating_pool)):
+        if randint(0, 100) >= 6:
+           mate_match.append(mating_pool[i])
+        else:
+            result.append(mating_pool[i])
+        if len(mate_match) == 2:
+            x, y = cross_over(mate_match)
+            result.append(x)
+            result.append(y)
+            mate_match = []
+    if len(mate_match) == 1:
+        result.append(mate_match[0])
+    return result
+
+def cross_over(mate_match):
+    cross_point = randint(0, k-1)
+    temp = 0
+    for i in range(0, cross_point):
+        temp = mate_match[0][i]
+        mate_match[0][i] = mate_match[1][i]
+        mate_match[1][i] = temp
+    x = mate_match[0]
+    y = mate_match[1]
+    return x, y;
+
+def mutation():
+    global pop
+    for i in range(0, len(pop)):
+        if randint(0, len(pop)-1) == 0:
+            for j in range(0, k):
+                if randint(0, k-1) == 0:
+                    if pop[i][j] == 0:
+                        pop[i][j] = 1
+                    else:
+                        pop[i][j] = 0
+                    
+
 
 def main():
-    global k
-    global smallest_string
-    global largest_string
+    max_fit = []
+    gen = 0
+    max_fit_last_beat = 0
+    global k, pop, smallest_string, largest_string
     string_one = input("enter string one \n")
     string_two = input("enter string two \n")
     #Determin which string is smaller and make k the length of the smaller string
@@ -73,7 +120,24 @@ def main():
     get_substrings()
     for i in range(0, population_size):
         pop[i][k+1] = fitness(pop[i], 0, 0, 0)
-    print(pop)
+    max_fit = pop[i]
+
+    while max_fit_last_beat < 10:
+        for i in range(0, population_size):
+            pop[i][k+1] = fitness(pop[i], 0, 0, 0)
+        print("population :\n" + str(pop))
+        print("\n") 
+        print("gen: " + str(gen) + " max_fit_last_beat : " + str(max_fit_last_beat) + "\n")
+        for i in range(0, population_size):
+            if max_fit[k+1] < pop[i][k+1]:
+                max_fit = pop[i]
+        new_pop = mating_pool(population_size)
+        pop = breed(new_pop)
+        gen += 1
+
+        max_fit_last_beat +=1
+    print("done : "  + str(max_fit))
+
 
 if __name__ == "__main__":
     main()
