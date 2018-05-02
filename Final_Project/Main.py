4904a55b90f8c29c32112c879bfb359b75e84f06
Elisha Waugh <elishawaugh@outlook.com>
2018-05-01 22:29:13 -0800

    new chages

--- Final_Project/Main.py
+++ Final_Project/Main.py
@@ -5,20 +5,25 @@ from tkinter import *
 import random
 import time
 import random
+import matplotlib.pyplot as plt
 
 
 
 def main():
-	pop_size = 1000
-	iterations = 1000000
+	pop_size = 100
+	iteration = 10000
 	max_fit = 0
 	top_fit = 0
 	average_fit = 0
 	pop = Population(pop_size, 10, 10)
 	pop.init_pop()
-	road = 5
+	road = 1
+	iterations = []
+	top_fits = []
+	max_fits = []
 
-	for i in range(iterations):
+	for i in range(iteration):
+		iterations.append(i)
 		
 		obstacle = -1
 		while(not pop.done()):
@@ -35,11 +40,21 @@ def main():
 		for j in range(pop_size):
 			fit_sum += pop.fit[j][1]
 		average_fit = fit_sum / pop_size
+		top_fits.append(top_fit)
+		max_fits.append(max_fit)
 		pop.breed(pop_size, 95, 2)
 		print("Iteration: " + str(i))
 		print("Max Fitness: " + str(max_fit))
 		print("Top Fitness: " + str(top_fit))
 		print("Average Fitness: " + str(average_fit))
+
+	plt.plot(top_fits, label= 'Top Fit')
+	plt.plot(max_fits, label = 'Max Fit')
+	plt.title("Fitness vs Time(Population: 100)")
+	plt.ylabel('Fitness')
+	plt.xlabel('Iterations')
+	plt.legend();
+	plt.show()
 
 if __name__=='__main__':
     main()
